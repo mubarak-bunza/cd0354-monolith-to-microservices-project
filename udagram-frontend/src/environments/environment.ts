d6eae85ba722fd87cc0d3aed40b9fb25e6836c70
Mubarak Abubakar Bunza <mubarakabubakar227@gmail.com>
2022-08-18 16:56:10 +0100

    update apiHost

--- udagram-frontend/src/environments/environment.ts
+++ udagram-frontend/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://aa039a74e4f25416d83e6249f7699b96-410565760.us-east-1.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
